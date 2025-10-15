4a190c7b37b56906fa783895d3007e47c327ef6d
Juan Francisco Cardona McCormick <Juan_Cardona@epam.com>
2025-10-15 11:03:07 -0500

    Adding the fresh project

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a example of usign JUnit on a toy project
+
+This is taken from the book Practical Unit Testing with JUnit and Mockito 
---
+++ src/main/java/org/jfcmc/junit/example/App.java
@@ -0,0 +1,10 @@
+package org.jfcmc.junit.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
